324bb8f76278e62783f9accbcaf7db9b4d02745c
Manish Mandal <manishmandalj@gmail.com>
2021-03-16 17:28:58 +0530

    minor changes

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT} `));
+app.listen(PORT, () => console.log(`server is running on port ${PORT} `));
